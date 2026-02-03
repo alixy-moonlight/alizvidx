b289137772d1c4a4d04c01633dc59f5ce395a2a7
alixy-moonlight <aceevsy@gmail.com>
2026-02-03 23:41:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-const API_KEY = "PUT_YOUR_KEY_HERE";
-const API_URL = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
-
-fetch(API_URL)
-  .then(res => res.json())
-  .then(data => showMovies(data.results));
-
-function showMovies(movies) {
-  const container = document.getElementById("movies");
-  container.innerHTML = "";
-
-  movies.forEach(movie => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
-      <p>${movie.title || movie.name}</p>
-    `;
-    container.appendChild(div);
+function searchMovies(text) {
+  const cards = document.querySelectorAll("#movies div");
+  cards.forEach(card => {
+    const title = card.innerText.toLowerCase();
+    card.style.display = title.includes(text.toLowerCase()) ? "block" : "none";
   });
 }
