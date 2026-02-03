867f4f3bdc3c0dae48a315d4a6367d2fa149baf7
alixy-moonlight <aceevsy@gmail.com>
2026-02-03 23:28:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const movies = [
   {
     title: "Heathers",
-    img: "https://upload.wikimedia.org/wikipedia/en/5/50/Heathers_film_poster.jpg",
-    link: "https://www.youtube.com/results?search_query=Heathers+1989+trailer"
+    image: "https://upload.wikimedia.org/wikipedia/en/5/50/Heathers_film_poster.jpg"
   },
   {
     title: "JoJo's Bizarre Adventure",
-    img: "https://upload.wikimedia.org/wikipedia/en/3/3f/JoJo_PB_DVD.jpg",
-    link: "https://www.youtube.com/results?search_query=JoJo+trailer"
+    image: "https://upload.wikimedia.org/wikipedia/en/6/6f/JoJo%27s_Bizarre_Adventure_Part_1.jpg"
   }
 ];
 
 const container = document.getElementById("movies");
 
-movies.forEach(m => {
-  container.innerHTML += `
-    <div>
-      <a href="${m.link}" target="_blank">
-        <img src="${m.img}">
-      </a>
-      <p>${m.title}</p>
-    </div>
+movies.forEach(movie => {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <img src="${movie.image}" width="120"><br>
+    ${movie.title}
   `;
+  container.appendChild(div);
 });
