83ac3df293fa2cca67a597d165c90a957a1b0a49
alixy-moonlight <aceevsy@gmail.com>
2026-02-03 23:20:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const movies = [
+  {
+    title: "Heathers",
+    img: "https://upload.wikimedia.org/wikipedia/en/5/50/Heathers_film_poster.jpg"
+  },
+  {
+    title: "JoJo's Bizarre Adventure",
+    img: "https://upload.wikimedia.org/wikipedia/en/2/2d/JoJo%27s_Bizarre_Adventure_logo.png"
+  }
+];
 
+const container = document.getElementById("movies");
+
+movies.forEach(movie => {
+  const div = document.createElement("div");
+  div.className = "movie";
+  div.innerHTML = `
+    <img src="${movie.img}">
+    <h3>${movie.title}</h3>
+  `;
+  container.appendChild(div);
+});
