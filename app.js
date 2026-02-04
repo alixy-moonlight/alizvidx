2d96e8a2d7a608aca9d7025d03bc1d9a515b16c4
alixy-moonlight <aceevsy@gmail.com>
2026-02-04 11:10:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Movies for each row
 const trending = [
     { title: "Heathers", img: "https://upload.wikimedia.org/wikipedia/en/5/50/Heathers_film_poster.jpg" },
     { title: "Avatar", img: "https://upload.wikimedia.org/wikipedia/en/d/d6/Avatar_%282009_film%29_poster.jpg" },
@@ -7,7 +7,7 @@ const trending = [
 
 const popular = [
     { title: "Stranger Things", img: "https://image.tmdb.org/t/p/w500/x2LSRK2Cm7MZhjluni1msVJ3wDF.jpg" },
-    { title: "The Boys", img: "https://image.tmdb.org/t/p/w500/ty5h5SeF0dr28aOmbf5tC6tpLr1.jpg" },
+    { title: "The Boys", img: "https://image.tmdb.org/t/p/w500/ty5h5SeF0dr28aOmbf5tC6tpLr1.jpg" }
 ];
 
 const topPicks = [
@@ -15,7 +15,7 @@ const topPicks = [
     { title: "Spider-Man: No Way Home", img: "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_No_Way_Home_poster.jpg" }
 ];
 
-// Function to add movies to the page
+// Add movies automatically to page
 function loadMovies(rowId, movies) {
     const container = document.getElementById(rowId);
     movies.forEach(movie => {
@@ -26,7 +26,7 @@ function loadMovies(rowId, movies) {
     });
 }
 
-// Load all rows
+// Display all rows
 loadMovies('trending', trending);
 loadMovies('popular', popular);
 loadMovies('topPicks', topPicks);
