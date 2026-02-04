dd639f1155ca7396f4d8488008bc553789bcc4c4
alixy-moonlight <aceevsy@gmail.com>
2026-02-04 10:56:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function searchMovies(text) {
-  const cards = document.querySelectorAll("#movies div");
-  cards.forEach(card => {
-    const title = card.innerText.toLowerCase();
-    card.style.display = title.includes(text.toLowerCase()) ? "block" : "none";
-  });
-}
+const movies = [
+  {
+    title: "Avatar",
+    image: "https://upload.wikimedia.org/wikipedia/en/d/d6/Avatar_%282009_film%29_poster.jpg"
+  },
+  {
+    title: "Heathers",
+    image: "https://upload.wikimedia.org/wikipedia/en/5/50/Heathers_film_poster.jpg"
+  }
+];
+
+const container = document.getElementById("movies");
+
+movies.forEach(movie => {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <img src="${movie.image}">
+    <p>${movie.title}</p>
+  `;
+  container.appendChild(div);
+});
